63660e324423478709f1e1db80d65e0ae8b05de7
Kay Cee <se.kaycee@gmail.com>
2023-05-05 10:54:08 +0100

    feat: handle resource caching

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """ Starts a Flash Web Application """
+import uuid
 from models import storage
 from models.state import State
 from models.city import City
@@ -34,7 +35,8 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('0-hbnb.html',
+                           cache_id=uuid.uuid4()
                            states=st_ct,
                            amenities=amenities,
                            places=places)
